b8c9c7c9a085e7bc9f87d49f57a6470be7f35c16
ljnsn <info@ljnsn.com>
2024-10-22 00:49:08 +0200

    dump: main

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use crustyfuzz::fuzz::*;
-
-fn main() {
-    let s1 = "this is a test";
-    let s2 = "this is a test!";
-    let score = ratio(Some(s1), Some(s2), None, None);
-    println!("{:?}", score);
-}
