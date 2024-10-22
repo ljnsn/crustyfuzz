1456ed1c8149880f273440434203c10824ba6ca7
ljnsn <info@ljnsn.com>
2024-10-23 01:31:24 +0200

    fix(models): implement score alignment methods

--- src/distance/models.rs
+++ src/distance/models.rs
@@ -1,10 +1,53 @@
+use pyo3::exceptions::PyIndexError;
 use pyo3::prelude::*;
 
-#[pyclass]
+#[pyclass(eq, mapping, get_all)]
+#[derive(PartialEq)]
 pub struct ScoreAlignment {
     pub score: f64,
     pub src_start: usize,
     pub src_end: usize,
     pub dest_start: usize,
     pub dest_end: usize,
+}
+
+#[derive(FromPyObject)]
+enum IndexResult {
+    #[pyo3(transparent, annotation = "int")]
+    Integer(usize),
+    #[pyo3(transparent, annotation = "float")]
+    Float(f64),
+}
+
+impl IntoPy<PyObject> for IndexResult {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        match self {
+            IndexResult::Integer(i) => i.into_py(py),
+            IndexResult::Float(f) => f.into_py(py),
+        }
+    }
+}
+
+#[pymethods]
+impl ScoreAlignment {
+    fn __len__(&self) -> usize {
+        5
+    }
+
+    fn __getitem__(&self, idx: isize) -> PyResult<IndexResult> {
+        let idx = if idx < 0 { 5 + idx } else { idx };
+
+        match idx {
+            0 => Ok(IndexResult::Float(self.score)),
+            1 => Ok(IndexResult::Integer(self.src_start)),
+            2 => Ok(IndexResult::Integer(self.src_end)),
+            3 => Ok(IndexResult::Integer(self.dest_start)),
+            4 => Ok(IndexResult::Integer(self.dest_end)),
+            _ => Err(PyIndexError::new_err("Opcode index out of range")),
+        }
+    }
+
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
 }
