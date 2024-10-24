7f78f40cab063e9e529294166dbdbdb86cc2d0b4
ljnsn <info@ljnsn.com>
2024-10-24 19:16:56 +0200

    fix: set module in init

--- python/crustyfuzz/__init__.py
+++ python/crustyfuzz/__init__.py
@@ -1,8 +1,10 @@
-"""Rusty string matching library."""
+# ruff: noqa: F405
+
+import sys
 
 from .crustyfuzz import *  # noqa: F403
 
-# ruff: noqa: F405
+sys.modules["crustyfuzz.distance"] = crustyfuzz.distance
 
 __doc__ = crustyfuzz.__doc__
 if hasattr(crustyfuzz, "__all__"):
